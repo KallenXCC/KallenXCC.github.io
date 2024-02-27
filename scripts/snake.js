d49cc23bb6d9b2ef8ac9afefcae6f0fe076330da
Braeton Buckley <braetonbuckley@gmail.com>
2024-02-27 12:55:58 -0800

    change apple color

--- scripts/snake.js
+++ scripts/snake.js
@@ -91,7 +91,7 @@ function loop() {
   context.clearRect(head.x, head.y, grid, grid);
 
   // draw apple
-  context.fillStyle = 'cyan';
+  context.fillStyle = '#66FFFF';
   context.fillRect(apple.x, apple.y, grid-1, grid-1);
 
   // draw snake one cell at a time
